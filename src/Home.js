1679a97627363909ddf7c62d0be2a311e1775e37
Munna Kumar <103780534+munna572000@users.noreply.github.com>
2022-06-17 18:11:02 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,42 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function Home() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=6004b8fcb1604003b4ead57854e8d2c2"
+      )
+      .then((response) => {
+        console.log(response);
+        setNews(response.data.articles);
+      });
+  });
+
   return (
-    <div>
-      <h1>home page</h1>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          {news.map((value, index) => {
+            return (
+              <div key={index} className="col-4">
+                <div className="card" style={{ width: "18rem" }}>
+                  <img
+                    src={value.urlToImage}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{value.title}</h5>
+                    <p className="card-text">{value.description}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
